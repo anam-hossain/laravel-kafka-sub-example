3436f3b22fe93be90d0cdec9de9df26699afb32f
Anam Hossain <anam.hossain@carsguide.com.au>
2019-12-03 17:37:42 +1100

    Create stats table

---
+++ app/Stat.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Stat extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'stats';
+}
---
+++ database/migrations/2019_12_03_060953_create_stats_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stats', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('inventory_id')->unsigned()->index();
+            $table->string('make')->nullable();
+            $table->string('model')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stats');
+    }
+}
