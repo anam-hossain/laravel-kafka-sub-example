46fe92cb1d2ab1eeb0ab8b327554af6565261737
Anam Hossain <anam.hossain@carsguide.com.au>
2019-12-19 16:38:32 +1100

    Composer update and Update title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Microservice 2 (Sub)
                 </div>
 
                 <div class="links">
